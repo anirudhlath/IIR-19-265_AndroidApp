45709fbdb7ba05d8f14f35de2d7cac479ae398d3
Anirudh Lath <anirudhlath@gmail.com>
2022-11-23 14:37:55 -0700

    Connected new fragments to the main activity

--- app/src/main/java/com/example/joriebutlerprojectnative/MainActivity.kt
+++ app/src/main/java/com/example/joriebutlerprojectnative/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.joriebutlerprojectnative
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.joriebutlerprojectnative.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun goToPatientActivity(view: View) {
+        startActivity(Intent(this@MainActivity, PatientActivity::class.java))
     }
 }
