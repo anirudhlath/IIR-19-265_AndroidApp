55b4d216ca75934295abac0e8f9921e46a090b0c
Anirudh Lath <anirudhlath@gmail.com>
2022-11-23 14:20:24 -0700

    Added patient activity and user sign up fragment

---
+++ app/src/main/java/com/example/joriebutlerprojectnative/PatientSignUpFragment.kt
@@ -0,0 +1,32 @@
+package com.example.joriebutlerprojectnative
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PatientSignUpFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PatientSignUpFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_patient_sign_up, container, false)
+    }
+
+
+}
