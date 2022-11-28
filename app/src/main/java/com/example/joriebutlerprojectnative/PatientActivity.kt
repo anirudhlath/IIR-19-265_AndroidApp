2fb6d1c922923bce20a2accfdecd0e1820239276
Anirudh Lath <anirudhlath@gmail.com>
2022-11-28 16:32:51 -0700

    Connected home page fragment to patient activity

--- app/src/main/java/com/example/joriebutlerprojectnative/PatientActivity.kt
+++ app/src/main/java/com/example/joriebutlerprojectnative/PatientActivity.kt
@@ -7,5 +7,6 @@ class PatientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient)
+
     }
 }
--- app/src/main/java/com/example/joriebutlerprojectnative/PatientSignUpFragment.kt
+++ app/src/main/java/com/example/joriebutlerprojectnative/PatientSignUpFragment.kt
@@ -1,10 +1,12 @@
 package com.example.joriebutlerprojectnative
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.joriebutlerprojectnative.databinding.ActivityMainBinding
+import com.example.joriebutlerprojectnative.databinding.ActivityPatientBinding
 
 
 /**
@@ -15,8 +17,10 @@ import android.view.ViewGroup
 class PatientSignUpFragment : Fragment() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -26,6 +30,12 @@ class PatientSignUpFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_patient_sign_up, container, false)
+    }
+
+    fun finishSetup(view: View) {
+        val fragment = PatientHomePageFragment()
+        parentFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment)
+
     }
 
 
