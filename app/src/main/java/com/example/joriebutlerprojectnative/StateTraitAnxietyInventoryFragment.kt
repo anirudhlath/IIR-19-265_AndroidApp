6c0cca0f62d2360e7d08bb1bdf88d9324edcb150
Anirudh Lath <anirudhlath@gmail.com>
2022-12-14 06:30:22 -0700

    Added STAI fragment

---
+++ app/src/main/java/com/example/joriebutlerprojectnative/StateTraitAnxietyInventoryFragment.kt
@@ -0,0 +1,29 @@
+package com.example.joriebutlerprojectnative
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StateTraitAnxietyInventoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StateTraitAnxietyInventoryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_state_trait_anxiety_inventory, container, false)
+    }
+
+}
