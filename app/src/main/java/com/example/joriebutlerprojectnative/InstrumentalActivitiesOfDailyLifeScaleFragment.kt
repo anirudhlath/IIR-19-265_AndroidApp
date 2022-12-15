51de319aee1bec70cf040f723d20705792a41c8e
anirudhlath <anirudhlath@gmail.com>
2022-12-15 07:13:34 -0700

    Added incomplete IADLS survey, needs work after more info is gathered.

---
+++ app/src/main/java/com/example/joriebutlerprojectnative/InstrumentalActivitiesOfDailyLifeScaleFragment.kt
@@ -0,0 +1,37 @@
+package com.example.joriebutlerprojectnative
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Compelete the layout and then implement this survey after Jories responds.
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InstrumentalActivitiesOfDailyLifeScaleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InstrumentalActivitiesOfDailyLifeScaleFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.questionnaire_instrumental_activities_of_daily_life_scale,
+            container,
+            false
+        )
+    }
+
+}
